6c98a8f94cdaf8e0cc12536ea01c57ebf1b999ca
Danish-7070 <danishsarfraz917@gmail.com>
2025-07-19 21:14:52 +0500

    Initial commit

--- Downloads/project-bolt-github-7k4bwssw/project/cjs/exams.cjs
+++
@@ -1,5 +0,0 @@
-@@ .. @@
- const express = require('express');
--const Exam = require('../server/models/Exam');
-+const Exam = require('../models/Exam.cjs');
- const { v4: uuidv4 } = require('uuid');
--- Downloads/project-bolt-github-7k4bwssw/project/cjs/results.cjs
+++
@@ -1,6 +0,0 @@
-@@ .. @@
- const express = require('express');
--const Response = require('../server/models/Response');
--const Exam = require('../server/models/Exam');
-+const Response = require('../models/Response.cjs');
-+const Exam = require('../models/Exam.cjs');
